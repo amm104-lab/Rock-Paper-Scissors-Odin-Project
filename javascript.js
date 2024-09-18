329dc3417e4ea0af2b0a1e86d7de8a996a6bea41
amm <amm@tutao.de>
2024-09-18 08:30:18 +0200

    wrote pseudocode and wrote function to get one of three numbers by random

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+//get a random number
+
+function getComputerChoice() {
+    return Math.round(Math.random() * 2);
+  }
+  
+  console.log(getComputerChoice())
+
+//if the number is 0 it should be rock, if the number is 1 it should be paper and if the number is 2 it should be scissors
+
+//get user input of rock, paper or scissors
+
+//compare user input to computers choice to see who won
+
+//output result
