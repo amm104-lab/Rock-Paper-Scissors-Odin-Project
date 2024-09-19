409a4b62f0b2047f93a826405c034602dc4265ae
amm <amm@tutao.de>
2024-09-19 08:12:13 +0200

    added counter for user and computer victory

--- javascript.js
+++ javascript.js
@@ -34,30 +34,36 @@ function playRound(humanChoice, computerChoice){
     }
     else if(humanChoice === "rock" && computerChoice === "paper"){
         console.log(computerChoice + "! you lose!")
+        computerScore++
     }
     else if(humanChoice === "rock" && computerChoice === "scissors"){
         console.log(computerChoice + "! you win!")
+        humanScore++
     }
     else if(humanChoice === "paper" && computerChoice === "rock"){
         console.log(computerChoice + "! you win!")
+        humanScore++
     }
     else if(humanChoice === "paper" && computerChoice === "scissors"){
         console.log(computerChoice + "! you lose!")
+        computerScore++
     }
     else if(humanChoice === "scissors" && computerChoice === "paper"){
         console.log(computerChoice + "! you win!")
+        humanScore++
     }
     else if(humanChoice === "scissors" && computerChoice === "rock"){
         console.log(computerChoice + "! you lose!")
+        computerScore++
     }
 }
 
 function playGame(){
     const humanChoice = getHumanChoice();
     const computerChoice = getComputerChoice();
-    const int = random();
+
 
     for(let i = 0; i < 5; i++) {
-        return playRound(humanChoice, computerChoice);
+        playRound(humanChoice, computerChoice);
     }
 }
