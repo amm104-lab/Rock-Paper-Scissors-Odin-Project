9cd3e800ce7974c19481635df22c2f3d61c7d025
amm <amm@tutao.de>
2024-09-30 17:08:35 +0200

    Added an UI with buttons while keeping the game functional

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+const res = document.querySelector("#result");
+const scr = document.querySelector("#score");
+const win = document.querySelector("#final");
 
 function random(){
     return Math.round(Math.random() * 2);
@@ -31,44 +34,105 @@ let computerScore = 0;
 let ties = 0
 
 function playRound(humanChoice, computerChoice){
-
-    if (humanChoice === computerChoice) {
-        console.log(computerChoice + "! Its a tie!")
-        ties++
+if(humanScore+computerScore+ties===5){
+    //declare winner
+    if(humanScore>computerScore){
+        win.textContent = "You win the game!"
     }
-    else if(humanChoice === "rock" && computerChoice === "paper"){
-        console.log(computerChoice + "! you lose!")
-        computerScore++
+    else if(computerScore>humanScore){
+        win.textContent = "You lose the game!"
     }
-    else if(humanChoice === "rock" && computerChoice === "scissors"){
-        console.log(computerChoice + "! you win!")
-        humanScore++
-    }
-    else if(humanChoice === "paper" && computerChoice === "rock"){
-        console.log(computerChoice + "! you win!")
-        humanScore++
-    }
-    else if(humanChoice === "paper" && computerChoice === "scissors"){
-        console.log(computerChoice + "! you lose!")
-        computerScore++
-    }
-    else if(humanChoice === "scissors" && computerChoice === "paper"){
-        console.log(computerChoice + "! you win!")
-        humanScore++
-    }
-    else if(humanChoice === "scissors" && computerChoice === "rock"){
-        console.log(computerChoice + "! you lose!")
-        computerScore++
+    else{
+        win.textContent = "It's a tie!"
     }
 }
 
-function playGame(){
+else{
+    if (humanChoice === computerChoice) {
+        /*console.log(computerChoice + "! Its a tie!")
+        ties++*/
+        res.textContent = computerChoice + "! It's a tie!";
+        ties ++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+    else if(humanChoice === "rock" && computerChoice === "paper"){
+        /*console.log(computerChoice + "! you lose!")
+        computerScore++*/
+        res.textContent = computerChoice + "! You lose!";
+        computerScore++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+    else if(humanChoice === "rock" && computerChoice === "scissors"){
+        /*console.log(computerChoice + "! you win!")
+        humanScore++*/
+        res.textContent = computerChoice + "! You win!";
+        humanScore++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+    else if(humanChoice === "paper" && computerChoice === "rock"){
+        /*console.log(computerChoice + "! you win!")
+        humanScore++*/
+        res.textContent = computerChoice + "! You win!";
+        humanScore++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+    else if(humanChoice === "paper" && computerChoice === "scissors"){
+        /*console.log(computerChoice + "! you lose!")
+        computerScore++*/
+        res.textContent = computerChoice + "! You lose!";
+        computerScore++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+    else if(humanChoice === "scissors" && computerChoice === "paper"){
+        /*console.log(computerChoice + "! you win!")
+        humanScore++*/
+        res.textContent = computerChoice + "! You win!";
+        humanScore++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+    else if(humanChoice === "scissors" && computerChoice === "rock"){
+        /*console.log(computerChoice + "! you lose!")
+        computerScore++*/
+        res.textContent = computerChoice + "! You lose!";
+        computerScore++;
+        scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    }
+}
+}
 
-    for(let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
+function playGame(humanChoice){
+
+    //for(let i = 0; i < 5; i++) {
+       // const humanChoice = getHumanChoice();
         const computerChoice = getComputerChoice();
         playRound(humanChoice, computerChoice);
-    }
+    //}
 
     console.log("You: "+humanScore+" CPU: "+computerScore+" Ties: "+ties)
 }
+
+
+
+const rock = document.querySelector("#Rock");
+rock.addEventListener("click", () => {
+    playGame("rock")
+})
+
+const paper = document.querySelector("#Paper");
+paper.addEventListener("click", () => {
+    playRound("paper",getComputerChoice())
+})
+
+const scissors = document.querySelector("#Scissors");
+scissors.addEventListener("click", () => {
+    playRound("scissors",getComputerChoice())
+})
+
+const reset = document.querySelector("#reset");
+reset.addEventListener("click", () => {
+    humanScore=0;
+    computerScore=0;
+    ties=0;
+    scr.textContent = "You: " + humanScore +" CPU: " + computerScore + " Ties: " + ties
+    win.textContent = "";
+})
